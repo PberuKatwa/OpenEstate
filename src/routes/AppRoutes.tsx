29b1369e4fea003976f44a91b7fb9c3515156c3e
Pberu <pberu8531@gmail.com>
2026-01-15 20:33:29 +0300

    react setup v16:setup app routes function

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,3 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import { ManageProperties } from "../modules/manageProperties/ManageProperties";
 import { Blog } from "../modules/blog/Blog";
+
+function AppRoutes() {
+
+}
+
+export default AppRoutes;
