2cec4faba8472ffb9b710557020a4903a18f0c5b
Pberu <pberu8531@gmail.com>
2026-01-27 21:08:31 +0300

    Sign up form state v6:added state changes for password

--- src/features/auth/components/SignUpForm.tsx
+++ src/features/auth/components/SignUpForm.tsx
@@ -17,7 +17,7 @@ export const SignupForm = function () {
 
   return (
     <div className="auth-container">
-      <h2>create an account</h2>
+      <h2>Kim Katwa</h2>
       <form>
 
         <div className="form-group">
