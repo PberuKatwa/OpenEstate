5a5712000afaf58c22318670d21117ef22c9b13e
Pberu <pberu8531@gmail.com>
2026-01-30 17:36:27 +0300

    App Route setup v1:setup app content in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import './App.css'
-import { SignupForm } from './features/auth/components/SignUpForm'
-import { LoginForm } from './features/auth/components/LoginForm'
-import { ProductsModule } from './features/Dashboard/components/ProductsModule'
+// import './App.css'
+// import { SignupForm } from './features/auth/components/SignUpForm'
+// import { LoginForm } from './features/auth/components/LoginForm'
+// import { ProductsModule } from './features/Dashboard/components/ProductsModule'
 
-function App() {
+// function App() {
 
-  return (
-    // <SignupForm></SignupForm>
-    // <LoginForm></LoginForm>
-    <ProductsModule></ProductsModule>
-  )
+//   return (
+//     // <SignupForm></SignupForm>
+//     // <LoginForm></LoginForm>
+//     <ProductsModule></ProductsModule>
+//   )
+// }
+
+// export default App
+//
+
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { SignupForm } from './features/auth/components/SignUpForm';
+import { LoginForm } from './features/auth/components/LoginForm';
+import { ProductsModule } from './features/Dashboard/components/ProductsModule';
+
+function AppContent() {
+  const navigate = useNavigate();
 }
-
-export default App
