1f4c6df3a19b60e41d1af3208e3be283aa9ed9b6
Pberu <pberu8531@gmail.com>
2026-01-30 18:05:45 +0300

    Api service setup v3:created sign up service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,16 @@ import { apiClient } from "./apiClient";
 
 export const authService = {
 
+  async signUp( firstName:string, lastName:string, email:string, password:string ) {
+    try {
+      const response = await apiClient.post("/auth/register", {
+        firstName,lastName,email,password
+      })
 
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 }
