c13660e2d4a7dfeb32bb99fbbddd6fa5c38f02a6
Pberu <pberu8531@gmail.com>
2026-01-31 17:00:20 +0300

    Dashboard layout v7:added sidebar to layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -5,6 +5,10 @@ export const DashboardLayout = function () {
   return (
     <div style={{display:"flex",minHeight:"100vh"}}>
       <Sidebar></Sidebar>
+
+      <main style={{ flex: 1, padding: "2rem", backgroundColor: "#f4f4f4" }}>
+        <Outlet/>
+      </main>
     </div>
   )
 }
