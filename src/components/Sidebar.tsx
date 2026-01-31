596158c8acb2ad4fe3cb6a71734828b1b1a636a7
Pberu <pberu8531@gmail.com>
2026-01-31 17:02:52 +0300

    Sidebar setup v1:adding nav items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,10 @@
+import { NavLink } from "react-router-dom"
+
 export const Sidebar = function () {
+  const navItems = [
+    { path:"/dashboard" }
+  ]
+
   return (
     <h1>THIS IS THE SIDEBARRR!!</h1>
   )
