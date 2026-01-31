8a55dda7d7a5aa027acfaaaa810a64f8231042cd
Pberu <pberu8531@gmail.com>
2026-01-31 16:52:21 +0300

    Dashboard layout v6:added sidebar to layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,1 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Sidebar } from "./Sidebar";
+
+export const DashboardLayout = function () {
+  return (
+    <div style={{display:"flex",minHeight:"100vh"}}>
+      <Sidebar></Sidebar>
+    </div>
+  )
+}
