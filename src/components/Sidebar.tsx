2002ea30bc154fd8e98e73dd3bba6c2f6b601bcf
Pberu <pberu8531@gmail.com>
2026-01-31 17:05:02 +0300

    Sidebar setup v2:added nav items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,11 @@ import { NavLink } from "react-router-dom"
 
 export const Sidebar = function () {
   const navItems = [
-    { path:"/dashboard" }
+    { path: "/dashboard", label: "Home" },
+    { path:"/dashboard/blogs", label:"Blogs" },
+    { path:"/dashboard/properties", label:"Properties" },
+    { path:"/dashboard/profile", label:"Profile" },
+
   ]
 
   return (
