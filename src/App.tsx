e4323b04d5e8b5851907c7df33b4d86aad73f5c1
Pberu <pberu8531@gmail.com>
2026-02-01 15:27:54 +0300

    clean up v2:removed reudndant files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-// import './App.css'
-// import { SignupForm } from './features/auth/components/SignUpForm'
-// import { LoginForm } from './features/auth/components/LoginForm'
-// import { ProductsModule } from './features/Dashboard/components/ProductsModule'
-
-// function App() {
-
-//   return (
-//     // <SignupForm></SignupForm>
-//     // <LoginForm></LoginForm>
-//     <ProductsModule></ProductsModule>
-//   )
-// }
-
-// export default App
-//
-
 import { BrowserRouter, Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import { SignupForm } from './features/auth/components/SignUpForm';
