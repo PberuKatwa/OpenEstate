f85e650260e372d6803a60bccd77a808e02baa21
Pberu <pberu8531@gmail.com>
2026-02-03 19:48:32 +0300

    blog service v1:added blogs service

---
+++ src/services/blogs.service.ts
@@ -0,0 +1,13 @@
+import { authorizedApiClient } from "./api.client";
+
+export const blogsService = {
+
+  async getAllBlogs(page: number, limit: number) {
+    try {
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+}
