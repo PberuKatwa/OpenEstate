60ac8c3cec76fffc545ef55e4f22de0e88c7c590
Pberu <pberu8531@gmail.com>
2026-02-03 19:52:55 +0300

    blog service v2:get all blogs

--- src/services/blogs.service.ts
+++ src/services/blogs.service.ts
@@ -1,9 +1,14 @@
+import type { AllBlogsApiResponse } from "../types/blog.types";
 import { authorizedApiClient } from "./api.client";
 
 export const blogsService = {
 
   async getAllBlogs(page: number, limit: number) {
     try {
+
+      const response = await authorizedApiClient.get(`/blogs/all/${page}/${limit}`);
+      const blogRes: AllBlogsApiResponse = response.data;
+      return blogRes;
 
     } catch (error) {
       throw error;
