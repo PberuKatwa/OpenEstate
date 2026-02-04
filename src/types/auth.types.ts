ba7a7b9946f07ddbae9bc49ed5a9b5b26628399b
Pberu <pberu8531@gmail.com>
2026-02-04 20:53:29 +0300

    auth types setup v2:setup auth context type

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,6 +1,17 @@
+import type { ApiResponse } from "./api.types";
+
 export interface User {
   firstName: string;
   lastName: string;
   signedUrl?: string;
   email: string;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  loading: boolean;
+  login: (credentials: { email: string; password: string }) => Promise<ApiResponse>;
+  logout: () => void;
 }
