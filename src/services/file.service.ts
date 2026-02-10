bdec69ff5be1492df13d306c25e74c1943f9342e
Pberu <pberu8531@gmail.com>
2026-02-10 20:43:22 +0300

    file service v2:created file types

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,1 +1,13 @@
 import { authorizedApiClient } from "./api.client";
+
+export const fileService = {
+
+  async uploadImage(payload: FormData) {
+    try {
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+}
