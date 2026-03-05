af6e607c78692c2f94a9bd59edae4d291588ff0d
Pberu <pberu8531@gmail.com>
2026-03-05 19:59:07 +0300

    cleanup

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -29,7 +29,7 @@ export const Profile = function () {
       setUserData(response.data);
 
     } catch (error) {
-      console.error(`Error in fethcing profile`, error)
+      console.error(`Error in fetching profile`, error)
       toast.error(`Error infetching profile`)
     } finally {
       setLoading(false)
